da1e782bf3ad181ae3d202f5802f80a81992bd4c
Rahul Sharan <123796550+rahuls512@users.noreply.github.com>
2023-08-10 17:02:56 +0530

    added internal alb for web and app.

--- alb.tf
+++ alb.tf
@@ -1,11 +1,12 @@
-############# External Application Load Balancer ################################################################################################
+############# Internet facing Application Load Balancer ################################################################################################
 resource "aws_lb" "this" {
-  name               = "rsinfotech-alb"
+  name               = "rsinfotech-internetFacing-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
   subnets            = [for each_subnet in aws_subnet.public_subnet : each_subnet.id]
 }
+
 ############# Target group ALB ################################################################################################
 resource "aws_lb_target_group" "this" {
   name     = "rsinfotech-alb-tg"
@@ -19,10 +20,11 @@ resource "aws_lb_target_group_attachment" "test" {
   target_id        = aws_instance.web.id
   port             = 80
 }
+
 ############# Listener ALB  ################################################################################################
 resource "aws_lb_listener" "this" {
   load_balancer_arn = aws_lb.this.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
@@ -30,39 +32,57 @@ resource "aws_lb_listener" "this" {
     target_group_arn = aws_lb_target_group.this.arn
   }
 }
-############# Internal Application Load Balancer ################################################################################################
-resource "aws_lb" "internal_alb" {
-  name               = "internal-alb"
+
+############# Internal facing Application Load Balancer ######################################################################
+resource "aws_lb" "i_alb" {
+  name               = "rsinfotech-internalFacing-alb" 
   internal           = true
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
+  security_groups   = [aws_security_group.web.id, aws_security_group.application_server.id]
+  subnets            = [for each_subnet in aws_subnet.private_subnet : each_subnet.id]
 }
-resource "aws_lb_target_group" "internal_target_group" {
-  name     = "web-app-tg"
+
+resource "aws_lb_target_group" "web_target_group" {
+  name     = "web-target-group"
   port     = 80
   protocol = "HTTP"
-}
-resource "aws_autoscaling_group" "internal_asg" {
-  name                 = "web-app-asg"
-  desired_capacity     = 0  
-  min_size             = 1
-  max_size             = 1
+  vpc_id   = aws_vpc.this.id
 
-  target_group_arns = [aws_lb_target_group.internal_target_group.arn]
-}
-
-resource "aws_lb_listener_rule" "internal_listener_rules" {
-  listener_arn = aws_lb.internal_alb.arn
-  priority     = 100
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.internal_target_group.arn
+  health_check {
+    protocol = "HTTP"
+    port     = "traffic-port"
   }
+}
 
-  condition {
-    path_pattern {
-      values = ["/"]
-    }
+resource "aws_lb_target_group" "app_target_group" {
+  name     = "app-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.this.id
+
+  health_check {
+    protocol = "HTTP"
+    port     = "traffic-port"
+  }
+}
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.i_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_target_group.arn
+  }
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.i_alb.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_target_group.arn
   }
 }
